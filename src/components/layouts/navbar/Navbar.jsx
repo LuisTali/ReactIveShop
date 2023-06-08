957a119171ddea9a6b5572eb19287a30aace2e7c
luisTali <luis3423@icloud.com>
2023-06-08 14:17:01 -0300

    Now if you click the brand image, it will redirect you to the homepage

--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -11,17 +11,13 @@ import LocalDrinkIcon from '@mui/icons-material/LocalDrink';
 const Navbar = ({theme,setTheme,productsInCart,setProductsInCart}) =>{
     const navigate = useNavigate();
 
-    const handleClick = () =>{
-        console.log('click');
-    }
-
     return <>
         <nav className={theme ? 'light' : 'dark'}>
-            <img src="https://res.cloudinary.com/dvcmeanik/image/upload/v1684852170/iuruqdqub5i4mgjw3cfs.jpg"/>
+            <img onClick={()=>navigate('/')} src="https://res.cloudinary.com/dvcmeanik/image/upload/v1684852170/iuruqdqub5i4mgjw3cfs.jpg"/>
             
             <Link to='/bebidas' className={theme ? 'light' : 'dark'}><LocalDrinkIcon/>Bebidas</Link>
             <Link to='/comidas' className={theme ? 'light' : 'dark'}><LunchDiningIcon/>Comidas</Link>
-            <a onClick={handleClick}><ElectricBoltIcon/>Tecnologia</a>
+            <a onClick={()=>console.log('click')}><ElectricBoltIcon/>Tecnologia</a>
             <a onClick={()=>setTheme(!theme)}><DarkModeIcon/>{theme ? 'DarkTheme' : 'LightTheme'}</a>
             <CardWidget theme={theme} productsInCart={productsInCart} setProductsInCart={setProductsInCart}/>
         </nav>
