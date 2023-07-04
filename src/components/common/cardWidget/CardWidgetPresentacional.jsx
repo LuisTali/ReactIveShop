d05ec793ced8cbae598872fe7b480b89e467d11f
Luis Taliercio <taliercioluis1@gmail.com>
2023-07-04 11:49:18 -0300

    Fixed styles to make responsive the endbuy page

--- src/components/common/cardWidget/CardWidgetPresentacional.jsx
+++ src/components/common/cardWidget/CardWidgetPresentacional.jsx
@@ -4,7 +4,7 @@ const CardWidgetPresentacional = ({productsInCart,handleRemove}) =>{
     return productsInCart.map((product)=> <li key={product.idCart}>
         <h3>{product.name}</h3>
         <b>${product.price}</b>
-        <b>{product.quantity}</b>
+        <b className="quantity">{product.quantity}</b>
         <button onClick={()=>handleRemove(product.idCart)}>remove</button>
     </li>)
 }
